07ea779e9b3e3ca17e5c3e3a7dccc03b2fb22f7c
Victor Mauroy <mauroy.victor@gmail.com>
2024-08-06 17:47:32 +0200

    feat: set up the folders and add basics files

---
+++ views/home/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Victor Mauroy - Portfolio</title>
+  <link rel='stylesheet' type='text/css' href='../../styles/style.css'>
+</head>
+
+<body>
+    <h1>Welcome to something</h1>
+</body>
+
+</html>
