2d6dab55ae35d2017b84d126f08ca093aab27d5f
VictorMauroy <mauroy.victor@gmail.com>
2024-09-04 11:45:27 +0200

    fix: fix incorrect navigation to section titles

--- components/navigation/navigation.php
+++ components/navigation/navigation.php
@@ -6,12 +6,8 @@
         <li class="nav-link current"><a href="#banner">Home</a></li>
         <li class="nav-link"><a href="#about">About</a></li>
         <li class="nav-link"><a href="#skills">Skills</a></li>
-        <li id="projects" class="nav-link"><a href="#projects">Projects</a></li>
-        <li id="games" class="nav-link"><a href="#Games">Games</a></li>
+        <li class="nav-link"><a href="#projects">Projects</a></li>
+        <li class="nav-link"><a href="#games">Games</a></li>
         <li class="nav-link"><a href="#Contact">Contact</a></li>
     </ul>
 </nav>
-
-
-<!-- Should add something to add space before next elements (to not hide them) --> 
-<!-- <div id="nav-height"></div> -->
