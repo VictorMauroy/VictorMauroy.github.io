387d7054d64b0f705cbe3606c88c3105b89b39de
VictorMauroy <mauroy.victor@gmail.com>
2024-09-15 11:38:26 +0200

    feat: add validation in php for name, email and message. Sanitize the values

--- components/contact/contact.php
+++ components/contact/contact.php
@@ -3,16 +3,87 @@
     $name = "";
     $email = "";
     $message = "";
+
+
+    $name_error = "";
+    $email_error = "";
+    $message_error = "";
     $request_status = "";
 
     if($_SERVER["REQUEST_METHOD"] == "POST") // Waiting for the form to be completed, then act. 
     {
-        // Do not forget to SANITIZE
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];
+        // Validation and sanitazing of the NAME input.
+        $name = $_POST['nom'];
+        $final_name = ""; // If every validation successfully ended, that variable will be set.
+        
+        $name_length = strlen($name);
+        if($name_length === 0) {
+            $name_error = "That field cannot be left empty.";
+        } 
+        else if($name_length < 4) {
+            $name_error = "Please, enter a valid name. Yours seems too short.";
+        } 
+        else if($name_length > 40) {
+            $name_error = "Please, enter a valid name. Yours seems too long.";
+        } 
+        else {
+            $no_tag_name = strip_tags($name);
+            $final_name = htmlspecialchars($no_tag_name, ENT_QUOTES, 'UTF-8');
+            $name_error = "";
+        }
 
-        $request_status = "The form has been successfully submitted.";
+
+        // Validation and sanitazing of the EMAIL input.
+        $email = $_POST['email'];
+        $final_email = "";
+
+        $email_length = strlen($email);
+        if($email_length === 0) {
+            $email_error = "That field cannot be left empty.";
+        } 
+        else if($email_length < 4) {
+            $email_error = "Please, enter a valid mail. Yours seems too short.";
+        } 
+        else if($email_length > 40) {
+            $email_error = "Please, enter a valid mail. Yours seems too long.";
+        } 
+        else {
+            $sanitized_email = filter_var($email, FILTER_SANITIZE_EMAIL);
+            if(filter_var($sanitized_email, FILTER_VALIDATE_EMAIL)) {
+                $final_email = $sanitized_email;
+                $email_error = "";
+            } else {
+                $email_error = "Please, enter a valid email. Unexpected email format.";
+            }
+        }
+
+        // Validation and sanitazing of the MESSAGE input.
+        $message = $_POST['message'];
+        $final_message = ""; // If every validation successfully ended, that variable will be set.
+        
+        $message_length = strlen($message);
+        if($message_length === 0) {
+            $message_error = "That field cannot be left empty.";
+        } 
+        else if($message_length < 20) {
+            $message_error = "Your message is quite short. Was it an error ?";
+        } 
+        else if($message_length > 1000) {
+            $message_error = "Your message seems too long. Can you reduce its length ?";
+        } 
+        else {
+            $no_tag_message = strip_tags($message);
+            $final_message = htmlspecialchars($no_tag_message, ENT_QUOTES, 'UTF-8');
+            $message_error = "";
+        }
+
+        if(!empty($final_name) && !empty($final_email) && !empty($final_message)) {
+            $request_status = "The form has been successfully submitted.";
+            
+        } else {
+            $request_status = "The form hasn't been submitted. There might be some errors with your informations.";
+        }
+
     }
 ?>
 
