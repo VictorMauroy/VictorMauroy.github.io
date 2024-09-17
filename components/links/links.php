58793014fc0f7eb08484e3cfbbccc7672b67178b
VictorMauroy <mauroy.victor@gmail.com>
2024-09-17 15:54:37 +0200

    remove unnecessary php files

--- components/links/links.php
+++
@@ -1,5 +0,0 @@
-<div class="personal-links">
-    <img src="../../assets/logos/github-logo.png" alt="Logo github" id="github-button"/>
-    <img src="../../assets/logos/linkedIn-logo.png" alt="Logo LinkedIn" id="linkedin-button"/>
-    <img src="../../assets/logos/gmail-logo.png" alt="Logo gmail" id="gmail-button"/>
-</div>
